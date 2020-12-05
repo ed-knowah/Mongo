a50bf95b0a6b8ce1325ad657ea6e3a65cca54491
ed_knowah <edetnoah@gmail.com>
2020-12-05 09:10:40 +0100

    committed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 const PORT = 4000;
 
 const peopleRoute = require('./routes/peopleRoute');
